f16c9c50f3157df7d082c7318b496c9cc0753ff5
Arthur De Witte <arthur.dewitte@gmail.com>
2020-05-01 00:37:38 +0200

    Test Bungee - Initial

---
+++ src/main/java/net/xiler/bungeenauncer/Main.java
@@ -0,0 +1,14 @@
+package net.xiler.bungeenauncer;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // You should not put an enable message in your plugin.
+        // BungeeCord already does so
+        getLogger().info("Yay! It loads!");
+    }
+}
+
